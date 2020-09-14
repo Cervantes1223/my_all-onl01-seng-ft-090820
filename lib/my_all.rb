1267a7c0e15da2e75e8aba05fb87e18df7d21f4c
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-14 22:46:59 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,7 +5,7 @@ require 'pry'
    while i < collection.length
    block_return_values <<  while i < collection.length 
    yield(collection[i])
-   i +=1 
+   i = i + 1 
  end 
 if block_return_values.include?(false)
     false
